d4881b467517fa09941d41e625a2b29c9aed3925
Konrad2 <39015360+Konrad22@users.noreply.github.com>
2021-11-18 11:43:45 +0100

    Splitting up the classes and main program

---
+++ mainprrogram.java
@@ -0,0 +1,12 @@
+	public static void main(String args[])
+	{
+		//has to run until game is over
+		//while true? maybe?
+		//do I have to end it, or can I just make a pop up that says 'side x lost' and make any moves on the board unviable by 
+		//emptying the list that contains the unoccupied places?
+		set free = empty list
+		
+		if color != player.color() or stone.set interesected with free == empty set
+			//stone not viable to move
+	}
+
