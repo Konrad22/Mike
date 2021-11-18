f73048229d5e12dcfed9846658c958c31ab0333e
Konrad2 <39015360+Konrad22@users.noreply.github.com>
2021-11-18 16:07:21 +0100

    implemented methods to query for internal variables

--- Gamepiece.java
+++ Gamepiece.java
@@ -1,32 +1,57 @@
 public class Gamepiece
 {
-	int x, y, r, c;				//c....color
-	Gamepiece(int newX, int newY, int newR)  //r...radian
+	int x, y, r;	
+	Color c;					//c....color
+	Gamepiece(int newX, int newY, int newR, Frame f)  //r...radian
 	{
 		x = newX;
 		y = newY;
 		r = newR;
 		c = BackgroundColor;		//needs to be implemented somewhere
-	}
-
-	void draw()
-	{
-		g.drawOval(x - r, y - r, 2*r, 2*r);		//is that possible? g would be the frame, I think?
+		f.this.draw()
 	}
 	
-	void erase()
+	Gamepiece(int newX, int newY, int newR, Color newC, Frame f)  //r...radian
 	{
-		this.c = BackgroundColor		//gotta implement it *somehow*
-		//
+		x = newX;
+		y = newY;
+		r = newR;
+		c = newC;		//needs to be implemented somewhere
+		f.this.draw()
+	}
+
+	void draw(int newC)
+	{
+		g.fillOval(x - r, y - r, 2*r, 2*r, newC);		//is that possible? g would be the frame, I think?
+	}
+	
+	void erase(Gamepiece g)
+	{
+		g.c = BackgroundColor;		//gotta implement it *somehow*
 	}
 	
 	void moveTo(int newX, int newY)
 	{
 		this.erase();
-		x = newX;
-		y = newy;
+		this.x = newX;
+		this.y = newy;
 		this.draw();
 	}
+	public Color c()
+	{
+		return c;
+	}
 	
+	public int x()
+	{
+		return x;
+	}
+	
+	public int y()
+	{
+		return y;
+	}	
 	
 }
+
+//probably will have to extend something somewhere to include coloring
