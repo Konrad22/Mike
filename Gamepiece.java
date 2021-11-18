d948141d904c4fc521ddf61db51d58fd9196bb4e
Konrad2 <39015360+Konrad22@users.noreply.github.com>
2021-11-18 11:41:19 +0100

    Splitting up the classes

---
+++ Gamepiece.java
@@ -0,0 +1,32 @@
+public class Gamepiece
+{
+	int x, y, r, c;				//c....color
+	Stone(int NewX, int newY, int newR)  //r...radian
+	{
+		x = newX;
+		y = newY;
+		r = newR;
+		c = BackgroundColor;		//needs to be implemented somewhere
+	}
+
+	void draw()
+	{
+		g.drawOval(x - r, y - r, 2*r, 2*r);		//is that possible? g would be the frame, I think?
+	}
+	
+	void erase()
+	{
+		this.c = BackgroundColor		//gotta implement it *somehow*
+		//
+	}
+	
+	void moveTo(int newX, int newY)
+	{
+		this.erase();
+		x = newX;
+		y = newy;
+		this.draw();
+	}
+	
+	
+}
