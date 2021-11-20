048c83b87b5ed3269ff77b94848d58cc77251b52
Konrad2 <39015360+Konrad22@users.noreply.github.com>
2021-11-20 14:32:21 +0100

    added method to check whose turn it is

--- Turncounter.java
+++ Turncounter.java
@@ -1,20 +1,32 @@
 public class Turncounter
 {
-  private boolean isTurnBlack = false;		//I probably still need a constructor for this to call the method, right?
+	private boolean isTurnBlack = false;		//I probably still need a constructor for this to call the method, right?
 	
 	Turncounter()														//Technically, this would construct a Turncounter with isTurnBlack == false ?
 	{
 	}
-  
-  public void changeTurn()
-  {
-    if isTurnBlack == false
-    {
-      isTurnBlack = true;
-    }
-    else
-    {
-      isTurnBlack = false;
+	
+	public void changeTurn()
+ 	{
+    		if isTurnBlack == false
+    		{
+      			isTurnBlack = true;
+    		}
+   		else
+    		{
+      			isTurnBlack = false;
+		}
+	}
+	
+	public boolean black()
+	{
+		if isTurnBlack == false
+    		{
+      			return false;
+    		}
+   		else
+    		{
+      			return true;
 		}
 	}
 }
