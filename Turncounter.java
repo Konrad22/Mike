91b2fd228614baac4f5afed7f35eb5507e72fd35
Konrad2 <39015360+Konrad22@users.noreply.github.com>
2021-11-20 13:57:05 +0100

    Necessary for game logic + boolean

---
+++ Turncounter.java
@@ -0,0 +1,20 @@
+public class Turncounter
+{
+  private boolean isTurnBlack = false;		//I probably still need a constructor for this to call the method, right?
+	
+	Turncounter()														//Technically, this would construct a Turncounter with isTurnBlack == false ?
+	{
+	}
+  
+  public void changeTurn()
+  {
+    if isTurnBlack == false
+    {
+      isTurnBlack = true;
+    }
+    else
+    {
+      isTurnBlack = false;
+		}
+	}
+}
